c1010b7e37d86aa9dc8ca00c2899befda36fd07c
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2024-09-27 13:39:07 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import Logo from './Logo'
+import { GrSearch } from 'react-icons/gr'
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <header className='h-16 shadow-md'>
+            <div className='h-full container mx-2 flex items-center px-4 justify-between'>
+                <div className=''>
+                    <Logo w={100} h={60} />
+                </div>
+
+                <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2'>
+                    <input type='text' placeholder='Search' className='w-full outline-none'/>
+                    <div className='text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full text-white'>
+                        <GrSearch />
+                    </div>
+                </div>
+
+                <div className=''>
+                    User Items
+                </div>
+            </div>
+        </header>
+    )
 }
 
 export default Header
